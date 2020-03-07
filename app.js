d6db433da76f52e8ccec97436da1864eb449e4e0
Rafał Karczmarzyk <liptunred@gmail.com>
2020-03-08 00:11:28 +0100

    Harvest all and plant all feature

--- app.js
+++ app.js
@@ -13,23 +13,32 @@ hackApp.classList.add('hack-app');
 if ( document.querySelector('.main_body') ) {
   document.querySelector('.main_body').appendChild(hackApp);
 }
+
+// water all
 const hackWaterAll = document.createElement('div');
 hackWaterAll.classList.add('hack-app__feature');
 hackWaterAll.innerHTML = `
   <h2>Water all</h2>
-  <button>Run</button>
+  <button onclick="hack.waterAll();">Run</button>
 `;
 hackApp.appendChild(hackWaterAll);
 
-hackWaterAll.addEventListener('click', () => {
-  selectMode(2, true, selected);
-  document.querySelectorAll('.feld').forEach( el => {
-    const field = getField(el);
-    if ( field.waterable ) {
-      el.click();
-    }
-  });
-});
+const hackHarvestAll = document.createElement('div');
+hackHarvestAll.classList.add('hack-app__feature');
+hackHarvestAll.innerHTML = `
+  <h2>Harvest all</h2>
+  <button onclick="hack.harvestAll();">Run</button>
+`;
+hackApp.appendChild(hackHarvestAll);
+
+const hackPlantAll = document.createElement('div');
+hackPlantAll.classList.add('hack-app__feature');
+hackPlantAll.innerHTML = `
+  <h2>Plant all</h2>
+  <button onclick="hack.plantAll();">Run</button>
+`;
+hackApp.appendChild(hackPlantAll);
+
 
 function trans(str) {
   const transStr = [];
@@ -38,24 +47,77 @@ function trans(str) {
   transStr['baumstumpf'] = 'stump';
   transStr['maulwurf'] = 'mole';
   transStr['steine'] = 'stone';
+  transStr['karotte'] = 'carrot';
+  transStr['salat'] = 'salad';
   return transStr[str] ? transStr[str] : str;
 }
 
 function getField(field) {
-  const plant = field
-    .querySelector('.plantImage')
-    .style.backgroundImage.split('"')[1]
-    .split('/')
-    .pop()
-    .replace('.gif', '')
-    .split('_')[0];
+  const imageFilename = field.querySelector('.plantImage').style.backgroundImage.split('"')[1].split('/').pop().replace('.gif', '');
+  const plant = imageFilename.split('_')[0];
+  const readyStatus = imageFilename.split('_').pop();
   const plantTrans = trans(plant);
   return {
-    waterable: !['mole','weeds','stump','stone','empty'].contains(plantTrans),
+    ready: readyStatus === '04' && !['mole','weeds','stump','stone'].contains(plantTrans),
     plant: plantTrans,
+    plantable: ['empty'].contains(plantTrans),
+    waterable: !['mole','weeds','stump','stone','empty'].contains(plantTrans),
   };
 }
 
-// document.querySelectorAll('.feld').forEach(el => {
-//   console.log(getField(el));
-// });
+const hack = {
+  clickByType(type, inverse = false) {
+
+  },
+  clickWaterable() {
+    console.log('click all waterable fields');
+    document.querySelectorAll('.feld').forEach( el => {
+      const field = getField(el);
+      if ( field.waterable ) {
+        el.click();
+      }
+    });
+  },
+  clickPlantable() {
+    console.log('click all plantable fields');
+    let index = 0;
+    document.querySelectorAll('.feld').forEach( el => {
+      const field = getField(el);
+      if ( field.plantable ) {
+        setInterval(() => {
+          el.click();
+        }, 200 * index);
+        index++;
+      }
+    });
+  },
+  clickHarvestable() {
+    console.log('click all harvestable fields');
+    document.querySelectorAll('.feld').forEach( el => {
+      const field = getField(el);
+      if ( field.ready ) {
+        el.click();
+      }
+    });
+  },
+  waterAll() {
+    selectMode(2, true, selected);
+    this.clickWaterable();
+  },
+  plantAll() {
+    selectMode(0,true,selected);
+    this.clickPlantable();
+  },
+  harvestAll() {
+    selectMode(1,true,selected);
+    this.clickHarvestable();
+  },
+  dump() {
+    document.querySelectorAll('.feld').forEach(el => {
+      console.log(getField(el));
+    });
+  }
+}
+
+
+// hack.dump();
