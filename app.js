190b35d170b278797c5b7cc193dc92643c383b36
Rafał Karczmarzyk <liptunred@gmail.com>
2020-03-08 21:46:56 +0100

    ✨ Detailed field info

--- app.js
+++ app.js
@@ -42,82 +42,130 @@ function trans(str) {
   return transStr[str] ? transStr[str] : str;
 }
 
-function getField(field) {
-  const imageFilename = field.querySelector('.plantImage').style.backgroundImage.split('"')[1].split('/').pop().replace('.gif', '');
+function getFieldInfo(fieldEl) {
+  const waterImage = fieldEl.querySelector('.wasser').src.split('/').pop().replace('.gif', '');
+  const imageFilename = fieldEl.querySelector('.plantImage').style.backgroundImage.split('"')[1].split('/').pop().replace('.gif', '');
   const plant = imageFilename.split('_')[0];
   const readyStatus = imageFilename.split('_').pop();
   const plantTrans = trans(plant);
   return {
+    x: parseInt(Array.from(fieldEl.classList).filter(el => el.includes('col'))[0].replace('col', '')),
+    y: parseInt(Array.from(fieldEl.classList).filter(el => el.includes('row'))[0].replace('row', '')),
     ready: readyStatus === '04' && !['mole','weeds','stump','stone'].contains(plantTrans),
     plant: plantTrans,
+    water: waterImage !== '0',
     plantable: ['empty'].contains(plantTrans),
     waterable: !['mole','weeds','stump','stone','empty'].contains(plantTrans),
+    el: fieldEl,
   };
 }
 
 
 
 
-const hack = {
-  clickByType(type, inverse = false) {
+class Hack {
+  constructor() {
+    this.gardenFields = [];
+    this.gardenPlants = [];
+    this.updateGardenStatus();
+    this.selectedFields = [];
+    this.updateStatusTimer = setInterval(() => {
+      this.updateGardenStatus();
+    }, 1000);
+    console.log(this.gardenPlants);
+  }
 
-  },
+  getField(x, y) {
+    const result = this.gardenFields.filter(el => (el.x === x && el.y === y));
+    return result[0]
+      ? result[0]
+      : false
+    ;
+  }
+
+  updateGardenStatus() {
+    this.gardenFields = [];
+    document.querySelectorAll('.feld').forEach(el => {
+      this.gardenFields.push(getFieldInfo(el));
+    });
+
+    this.gardenPlants = []
+    this.gardenFields.forEach(el => {
+      if ( typeof this.gardenPlants[el.plant] === 'undefined' ) {
+        this.gardenPlants[el.plant] = {
+          name: el.plant,
+          total: 0,
+          ready: 0,
+          growing: 0,
+          watered: 0,
+          fields: []
+        };
+      }
+      this.gardenPlants[el.plant].total += 1;
+      if (el.ready) {
+        this.gardenPlants[el.plant].ready += 1;
+      } else {
+        this.gardenPlants[el.plant].growing += 1;
+      }
+      if (el.water) {
+        this.gardenPlants[el.plant].watered += 1;
+      }
+      this.gardenPlants[el.plant].fields.push(el);
+    });
+  }
 
   clickWaterable() {
     console.log('click all waterable fields');
     document.querySelectorAll('.feld').forEach( el => {
-      const field = getField(el);
+      const field = getFieldInfo(el);
       if ( field.waterable ) {
         el.click();
       }
     });
-  },
+  }
 
   clickPlantable() {
     console.log('click all plantable fields');
-    let index = 0;
     document.querySelectorAll('.feld').forEach( el => {
-      const field = getField(el);
+      const field = getFieldInfo(el);
       if ( field.plantable ) {
-        setInterval(() => {
-          el.click();
-        }, 200 * index);
-        index++;
+        el.click();
       }
     });
-  },
+  }
 
   clickHarvestable() {
     console.log('click all harvestable fields');
     document.querySelectorAll('.feld').forEach( el => {
-      const field = getField(el);
+      const field = getFieldInfo(el);
       if ( field.ready ) {
         el.click();
       }
     });
-  },
+  }
 
   waterAll() {
     selectMode(2, true, selected);
     this.clickWaterable();
-  },
+  }
 
   plantAll() {
     selectMode(0,true,selected);
     this.clickPlantable();
-  },
+  }
 
   harvestAll() {
     selectMode(1,true,selected);
     this.clickHarvestable();
-  },
+  }
 
   dump() {
     document.querySelectorAll('.feld').forEach(el => {
-      console.log(getField(el));
+      console.log(getFieldInfo(el));
     });
   }
 }
 
+const hack = new Hack;
 
 // hack.dump();
